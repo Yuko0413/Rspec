e70ca4b028dced4378727cae47d955b00b8cb853
Yuko0413 <soc13soc0422@gmail.com>
2024-06-17 14:06:52 +0900

    初回

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :status, presence: true
   validates :deadline, presence: true
-  validate :deadline, :must_start_from_today
+  validate :must_start_from_today
 
   def must_start_from_today
     errors.add(:deadline, 'must start from today.') if deadline.present? && deadline < Date.current
